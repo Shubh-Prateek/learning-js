7854ae1521d1c5eb0001959f29c443f04a3b2e2e
Shubh-Prateek <147850011+Shubh-Prateek@users.noreply.github.com>
2023-10-13 17:22:58 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,30 @@
+const accountId =144553
+let accoutnEmail="shubh@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+
+//accountId=2 //not allowed
+console.log(accountId);
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+let accountState;//undefined aaya
+console.table([accountId,accoutnEmail,accountPassword,accountCity,accountState])
+
+
+/*ye keyword mile to ye kro
+const mtlb isko change na kro
+
+console.table se tabular form m data dikha
+let se memory m space assign hogya var se v same
+
+accountCity na var na let to kaise hua
+
+var use nhi krte kyuki scope ka isko nhi pta tha
+{ }isko scope bolte
+logscope p control nhi tha
+maanlo name variable firse use kiya to change hojata tha
+
+//javascript mein bina variable ke aage kuch likhe variable ki memory reserve krskte tabhi accountCity
+*/
