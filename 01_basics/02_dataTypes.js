15d020cec2d9aa868b8b90138895bdc605687ff2
Shubh-Prateek <147850011+Shubh-Prateek@users.noreply.github.com>
2023-10-13 17:48:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+/*purana code kharab na ho
+uske liye "use strict";//treat all JS code as newer version 
+ek baar likh diya to puri file m naye standard ki tarah treat hoga code machine ke hissab se
+
+alert("hello") chala nhi  kyuki ye browser p hi rhta node p use krne ka alert ko diff syntax hai
+
+code readable aur futuristic hona chahiye
+original documentation js ka mdn nhi hai tc39.es
+alag browser ke alg standard they ==tb ecma aaya
+
+number =>2 to power 53 range, bigint, string, boolean, null=>standalone value, undefined,symbol
+null represent khaali, unique ke liye symbol use krte   kaafi jyada react mein
+ */
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+console.log(typeof 3);
+console.log(typeof "DHol");
